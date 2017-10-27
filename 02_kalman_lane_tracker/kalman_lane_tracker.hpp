5d7b31f9b137bacf2b041195ba0b261a61b38784
Weiyuan <ax61316@hotmail.com>
2017-10-27 12:20:22 +0800

    Finish constructor of class KalmanLaneTracker

--- 02_kalman_lane_tracker/kalman_lane_tracker.hpp
+++ 02_kalman_lane_tracker/kalman_lane_tracker.hpp
@@ -1,5 +1,5 @@
-# ifndef KALMAN_LANE_TRACKER_H
-# define KALMAN_LANE_TRACKER_H
+#ifndef KALMAN_LANE_TRACKER_H
+#define KALMAN_LANE_TRACKER_H
 
 #include <vector>
 #include <string.h>
@@ -7,77 +7,96 @@
 #include "opencv2/video/tracking.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
+// performance boost: Matrix access
 class KalmanLaneTracker
 {
 protected:
-    int _n_lanes;
-    int _meas_size;
-    int _state_size;
-    int _contr_size = 0;
+    int  _n_lanes;
+    int  _meas_size;
+    int  _state_size;
+    int  _contr_size = 0;
     bool _first_detected;
 
 public:
+    // constructor
     KalmanLaneTracker(int _n_lanes, float proc_noise_scale, float meas_noise_scale)
     {
-        float process_cov_parallel = 0.0;
-        char proc_noise_type[] = "white";
-        _meas_size  = 4 * _n_lanes;
-        _state_size = 2 * _meas_size;
-        
-        std::cout << "n_lanes: " << _n_lanes << ", meas_size: "  << _meas_size << 
-        ", state_size: " << _state_size << ", _contr_size: "  << _contr_size << std::endl;
-        
-        cv::KalmanFilter kf(_state_size, _meas_size, _contr_size);        
-        
-        kf.transitionMatrix = cv::Mat::eye(_state_size, _state_size, CV_32F); // dtype = 32 bit float        
+        float process_cov_parallel = 0;
+        char  proc_noise_type[]    = "white";
+        _meas_size                 = 4 * _n_lanes;
+        _state_size                = 2 * _meas_size;
+
+        std::cout << "n_lanes: " << _n_lanes << ", meas_size: " << _meas_size << ", state_size: " << _state_size
+                  << ", _contr_size: " << _contr_size << std::endl;
+
+        cv::KalmanFilter kf(_state_size, _meas_size, _contr_size);
+
+        kf.transitionMatrix  = cv::Mat::eye(_state_size, _state_size, CV_32F); // dtype = 32 bit float
         kf.measurementMatrix = cv::Mat::zeros(_meas_size, _state_size, CV_32F);
-                        
-        for(int i = 0; i < sizeof(_meas_size); i++)
-        {
-            kf.measurementMatrix.at<float>(i, i*2) = 1; // access point (i, i*2) and assign it to 1;
-        }                
-        
-        if( strncmp( proc_noise_type, "white", 2) == 0) // 2 -> compare two elements
-        {
-            cv::Mat block = (cv::Mat_<float>(2,2) << 0.25, 0.5, 0.5, 1);
-            //kf.processNoiseCov = block_diag(*([block] * _meas_size)) * proc_noise_scale
+
+        for (int i = 0; i < sizeof(_meas_size); i++) {
+            kf.measurementMatrix.at<float>(i, i * 2) = 1; // access point (i, i*2) and assign it to 1;
         }
-        if( strncmp( proc_noise_type, "identity", 2) == 0)
+
+        if (strncmp(proc_noise_type, "white", 2) == 0) // 2 -> compare two elements
         {
+            int     size_block = 2;
+            cv::Mat block      = (cv::Mat_<float>(size_block, size_block) << 0.25, 0.5, 0.5, 1);
+            std::cout << "block: \n" << block << std::endl;
+
+            // assignment of kf.processNoiseCov matrix
+            kf.processNoiseCov = cv::Mat::zeros(size_block * _meas_size, size_block * _meas_size, CV_32F) * proc_noise_scale;
+            for (int i = 0; i < _meas_size *2 ; i+=2) // 0, 2, 4, 6
+            {                
+                for (int j = 0; j < 2; j++) // 0 ,1
+                {             
+                    for(int k = 0; k < 2; k++) // 0 ,1
+                    {                                         
+                        kf.processNoiseCov.at<float>(i+k,i+j) = block.at<float>(k, j);
+                    }                    
+                }
+            }            
+        }
+        if (strncmp(proc_noise_type, "identity", 2) == 0) {
             kf.processNoiseCov = cv::Mat::eye(_state_size, _state_size, CV_32F);
         }
-        for(int i=0; i< _meas_size; i+=2)
+        for (int i = 0; i < _meas_size; i += 2) 
         {
-            for(int j=0; j< _n_lanes; j++)
+            for (int j = 1; j < _n_lanes; j++) // 1 ???
             {
-                //kf.processNoiseCov ...
+                std::cout << "(x, y)" << "(" << i << "," << i+(j*8) << ")" << std::endl;
+                std::cout << "(x, y)" << "(" << i+(j*8) << "," << i << ")" << std::endl;
+                kf.processNoiseCov.at<float>(i, i+(j*8)) = process_cov_parallel; 
+                kf.processNoiseCov.at<float>(i+(j*8), i) = process_cov_parallel;                 
             }
-        }        
+        }
+        std::cout << kf.processNoiseCov << std::endl;
 
- /*       kf.measurementNoiseCov = cv::Mat::eye(_meas_size, _meas_size, CV_32F);
-        kf.errorCovPre = cv::Mat::eye(_state_size, _state_size, CV_32F);
-        
-        cv::Mat meas = cv::Mat::zeros(_meas_size, 1, CV_32F);        
-        cv::Mat state = cv::Mat::zeros(_state_size, 1, CV_32F);        
+        kf.measurementNoiseCov = cv::Mat::eye(_meas_size, _meas_size, CV_32F);
+        kf.errorCovPre         = cv::Mat::eye(_state_size, _state_size, CV_32F);
+
+        cv::Mat meas  = cv::Mat::zeros(_meas_size, 1, CV_32F);
+        cv::Mat state = cv::Mat::zeros(_state_size, 1, CV_32F);
 
         _first_detected = false;
-*/        
     }
-    /*
-    float _update_dt(float dt)
+    
+    float _update_dt(cv::KalmanFilter& kf, float dt)
     {
-        for (int i = 0; i < state_size; i+=2) {
-            kf.measurementMatrix.at<float>(i, i+1) = dt;
+        for (int i = 0; i < _state_size; i+=2) {
+            kf.transitionMatrix.at<float>(i, i+1) = dt;
         }
     }
-    float _first_detect(int lanes) 
+    // Mat state is defined in constructor
+    float _first_detect(cv::KalmanFilter& kf, cv::Mat& state, int lanes)
     {
-        for l, i in zip(lanes, range(0, state_size, 8)):
-            state[i:i+8:2, 0] = l
+        /*for l, i in zip(lanes, range(0, _state_size, 8)):
+            kf.state[i:i+8:2, 0] = l */
+        
         kf.statePost = state;
-        first_detected = true;
+        _first_detected = true;
     }
-    float update(int lanes) 
+/*    float update(int lanes)
     {
         if(first_detected) // if first_detected = true
         {
@@ -85,11 +104,11 @@ public:
             {
                 if(l != NULL)
                     meas[i:i+4, 0] = l;
-            }            
+            }
             kf.correct(meas);
         }
     }
-    float predict(float dt) 
+    float predict(float dt)
     {
         if(first_detected)
         {
@@ -107,4 +126,4 @@ public:
     }
     */
 };
-# endif
+#endif
