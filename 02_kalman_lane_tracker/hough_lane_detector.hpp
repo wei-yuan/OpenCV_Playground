52905103b93525057c86bd612ec3aca1248c0e01
Weiyuan <ax61316@hotmail.com>
2017-10-28 18:38:49 +0800

    Finish for loop for finding lane information of detect function in class HoughLaneDetector

--- 02_kalman_lane_tracker/hough_lane_detector.hpp
+++ 02_kalman_lane_tracker/hough_lane_detector.hpp
@@ -1,6 +1,7 @@
 #ifndef HOUGH_LANE_DETECTOR_H
 #define HOUGH_LANE_DETECTOR_H
 
+#include <math.h>       /* atan2 */
 #include <vector>
 #include <utility>
 #include <iostream>
@@ -45,7 +46,7 @@ public:
         cv::HoughLines(frame, lines, 1, CV_PI / 180, init_vote, 0, 0);
 
         cv::Point pt1, pt2;
-        auto      l = {pt1, pt2};
+        //auto      l = {pt1, pt2};
 
         for (auto& line : lines) {
             float rho   = line[0];
@@ -87,6 +88,9 @@ public:
     {
         float     m; // slope value m
         cv::Point pt1, pt2;
+        // assignment
+        pt1.x = x1; pt1.y = y1;
+        pt2.x = x2; pt2.y = y2;
 
         if (pt1.x == pt1.x) {
             if (pt1.y < y2) {
@@ -125,10 +129,9 @@ public:
         return {pt1, pt2};
     }
 
-    void detect(cv::UMat& frame)
-    {
-        std::cout << "HoughLaneDetector detect" << std::endl;        
-        
+    std::pair<std::vector<cv::Vec4f>, std::vector<cv::Vec4f>>
+    detect(cv::UMat& frame)
+    {                
         cv::UMat gray, croppedImg, dst, cdst, blur, contour;
         cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
         // w = frame.shape[0], h = frame.shape[1]
@@ -137,37 +140,31 @@ public:
 
         // crop input image
         // cv::Rect roi(xMin,yMin,xMax-xMin,yMax-yMin);
+        //cv::Rect roi(_road_horizon, 0, (roiy_end - _road_horizon), (roix_end - 0));
         cv::Rect myROI(500, 300, 100 * 4, 100 * 3);
         cv::UMat croppedRef(frame, myROI);
         croppedRef.copyTo(croppedImg);
-        // edge detection
-/*        cv::Canny(croppedImg, dst, 50, 200, 3);        
-        cv::Rect roi(_road_horizon, 0, (roiy_end - _road_horizon), (roix_end - 0));
-        cv::UMat croppedRef(gray, roi);
-        croppedRef.copyTo(croppedImg);
-*/        
         // blurring
         int ksize = 5;
-        medianBlur(croppedImg, blur, ksize);
+        cv::medianBlur(croppedImg, blur, ksize);
         // edge detection
         cv::Canny(blur, contour, 60, 120);                        
         
-        std::vector<cv::Vec2f> lines;
+        //std::vector<cv::Vec2f> lines;
         std::vector<cv::Vec4f> plines;
         std::pair<cv::Point, cv::Point> houghlines;                
         
         _prob_hough = true;        
         if (_prob_hough) // true
-        {            
-            std::cout << "_prob_hough: " << _prob_hough << std::endl;
+        {                        
             // void HoughLinesP(InputArray image, OutputArray lines, 
             //                  double rho, double theta, int threshold, 
             //                  double minLineLength=0, double maxLineGap=0)
             // type of lines = std::vector<cv::Vec4f>, i.e. vector with 4 float element            
-            cv::HoughLinesP(contour, plines, 1, CV_PI / 180, _vote, 30, 100);
+            cv::HoughLinesP(contour, plines, 1, CV_PI / 180, _vote, 0, 0);
            
             cv::Point pt1, pt2;
-            auto      l = {pt1, pt2};
+            //auto      l = {pt1, pt2};
     
             for (auto& line : plines) {
                 float rho   = line[0];
@@ -185,34 +182,77 @@ public:
             // prepare the color canvas for output image 
             cv::cvtColor(contour, cdst, CV_GRAY2BGR);            
             // draw line
-            houghlines = {pt1, pt2};
+            houghlines = {pt1, pt2};            
             cv::line(cdst, houghlines.first, houghlines.second, cvScalar(0, 0, 255), 3, cv::LINE_AA);                        
             // generate line with std::pair<cv::Point, cv::Point> format
             std::cout << "class member function detect. first point: "<< pt1 
             <<  ", second point: "<< pt2 << std::endl;
-            cv::imshow("line using HoughLineP", cdst);
-            cv::waitKey();
+            //cv::imshow("line using HoughLineP", cdst);
+            //cv::waitKey();
         } 
         else 
         {
             // self defined function, return std::pair<cv::Point, cv::Point>
             houghlines = _standard_hough(dst, _vote);
-        }
-        //     
-        /*if(houghlines)
-        {
-            auto l = {pt1, pt2};
-            for(auto& line : lines) {
-
-            }
-        }
-        else
-        {
-            throw HoughLaneDetector;
-        }*/
+        } 
         
+        // find nearest lines to center
+//        lines = lines+np.array([0, self.road_horizon, 0, self.road_horizon]).reshape((1, 1, 4))
+        // scale points from ROI coordinates to full frame coordinates
+        std::vector<cv::Vec4f> left_bound, right_bound;
+        // find the rightmost line of the left half of the frame
+        // and the leftmost line of the right half
+        cv::Point pt1, pt2;        
+        float theta, left_dist, right_dist;
+        for (auto& line : plines)
+        {
+            theta = std::abs(atan2((pt2.y - pt1.y), (pt2.x-pt1.x)));
+            if (theta > _roi_theta)
+            {
+                float dist = _base_distance(pt1.x, pt1.y, pt2.x, pt2.y, frame.cols);
+                if(dist < 0)
+                {
+                    left_bound[0] = pt1.x;
+                    left_bound[1] = pt1.y;
+                    left_bound[2] = pt2.x;
+                    left_bound[3] = pt2.y;
+                    left_dist = dist;
+                }
+                else if(dist > 0)
+                {
+                    right_bound[0] = pt1.x;
+                    right_bound[1] = pt1.y;
+                    right_bound[2] = pt2.x;
+                    right_bound[3] = pt2.y;
+                    right_dist = dist;
+                }
+                else if(0 > dist > left_dist)
+                {
+                    left_bound[0] = pt1.x;
+                    left_bound[1] = pt1.y;
+                    left_bound[2] = pt2.x;
+                    left_bound[3] = pt2.y;
+                    left_dist = dist;
+                }
+                else if(0 > dist > right_dist)
+                {
+                    right_bound[0] = pt1.x;
+                    right_bound[1] = pt1.y;
+                    right_bound[2] = pt2.x;
+                    right_bound[3] = pt2.y;
+                    right_dist = dist;
+                }
+            }
+        }    
+        // _scale_line return std::pair<cv::Point, cv::Point>
+        // if left_bound != NULL
+        std::pair<cv::Point, cv::Point> scale_pair_left, scale_pair_right;
+        
+        //scale_pair_left = _scale_line(left_bound[0], left_bound[1], left_bound[2], left_bound[3], frame.rows);
+        //scale_pair_right = _scale_line(right_bound[0], right_bound[1], right_bound[2], right_bound[3], frame.rows);
+        // conversion here
 
-//        return {left_bound, right_bound};
+        return {left_bound, right_bound};
     }
 };
 #endif
