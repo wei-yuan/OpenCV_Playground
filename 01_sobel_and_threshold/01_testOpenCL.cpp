bb4dee4ab620645f992f4d990d9ec73373c41966
Weiyuan <ax61316@hotmail.com>
2017-10-31 21:02:38 +0800

    Add python profile example

--- 01_sobel_and_threshold/01_testOpenCL.cpp
+++ 01_sobel_and_threshold/01_testOpenCL.cpp
@@ -10,41 +10,41 @@ using namespace std;
 int main()
 {
     // cap is the object of class video capture that tries to capture Bumpy.mp4
-    VideoCapture cap("../test2_720p.mp4");
-	
+    cv::VideoCapture cap("/home/alex504/img_video_file/kalman/road_view.mp4");
+    // cv::VideoCapture cap("/home/alex/img_video_file/road_view.mp4");
+
     if ( !cap.isOpened() )  // isOpened() returns true if capturing has been initialized.
     {
 		cout << "Cannot open the video file. \n";
 		return -1;
     }
 
-    cout << "Have OpenCL?: " << cv::ocl::haveOpenCL() << endl;
-
-/*  SWITCH OPENCL ON/OFF IN LINE BELLOW */
-    ocl::setUseOpenCL(true);
-/*                                      */
-    int nBlurs = 50;
-
-    long frameCounter = 0;
-
+    // count time 
     std::time_t timeBegin = std::time(0);
     int tick = 0;
 
+    // OpenCL related
+    cout << "Have OpenCL?: " << cv::ocl::haveOpenCL() << endl;
+    /*  SWITCH OPENCL ON/OFF IN LINE BELLOW */
+    ocl::setUseOpenCL(true);    
+
+    // sobel filter
+    int nBlurs = 50;
+    long frameCounter = 0;
+        
     cv::UMat frame;
     cv::UMat frameGray;
     cv::UMat frameSobelx;
-    cv::UMat frameSobely;
-
+    cv::UMat frameSobely;    
     cv::UMat frameSobel;
     cv::UMat blurredSobel;
-    /*cv::Mat frame;
-    cv::Mat frameGray;
-    cv::Mat frameSobelx;
-    cv::Mat frameSobely;
-
-    cv::Mat frameSobel;
-    cv::Mat blurredSobel;*/
-    //cv::UMat Thresh;
+    
+    // thresh
+    int thresh_val = 125;
+    int max_BINARY_value = 256;
+    int threshold_type = THRESH_TOZERO_INV;
+    
+    cv::UMat thresh;
 
     while (1)
     {
@@ -52,25 +52,27 @@ int main()
             cout<<"\n Cannot read the video file. \n";
             break;
         }
-
+        
+        // RGB to GRAY
         cv::cvtColor(frame, frameGray, cv::COLOR_BGR2GRAY);
-
+        // Sobel
         cv::Sobel(frameGray, frameSobelx, frameGray.depth(), 1, 0, 3);
         cv::Sobel(frameGray, frameSobely, frameGray.depth(), 0, 1, 3);
-
         cv::bitwise_or(frameSobelx, frameSobely, frameSobel);
-
         for (int n = 0; n < nBlurs; n++)
             cv::blur(frameSobel, blurredSobel, cv::Size(3,3));
+        // Threshold
+        threshold( blurredSobel, thresh, thresh_val, max_BINARY_value, threshold_type );
 
-        //cv::imshow("Original Frame", frame);            
+        cv::imshow("Original Frame", frame);            
         cv::imshow("Sobel blurred Frame", blurredSobel);
+        cv::imshow("Threshold", thresh);
+        
         cv::waitKey(1);
-
+        
+        // calculate time 
         frameCounter++;
-
         std::time_t timeNow = std::time(0) - timeBegin;
-
         if (timeNow - tick >= 1)
         {
             tick++;
---
+++ 01_sobel_and_threshold/objs/01_testOpenCL.d
@@ -0,0 +1,1 @@
+objs/01_testOpenCL.o: 01_testOpenCL.cpp
