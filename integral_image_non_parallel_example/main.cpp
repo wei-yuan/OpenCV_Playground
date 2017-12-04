6cd9b58ebdfad0b5c83207ef8891d8eaa4038f7e
wei-yuan <ax61316@hotmail.com>
2017-12-04 16:37:23 +0800

    seperate integral image tp two parts

---
+++ integral_image_non_parallel_example/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/ocl.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>\
+
+int main()
+{    
+    cv::Mat src = cv::imread("720p.jpg", CV_32F);
+    cv::Mat src_sum, src_sqsum;
+    
+    cv::UMat usrc, usrc_sum, usrc_sqsum;
+    src.copyTo(usrc);
+
+    // Time measurement
+    int sdepth = -1;
+    double mat_t1 = (double)cv::getTickCount();
+        cv::integral(src, src_sum, src_sqsum, sdepth);
+    double mat_t2 = (double)cv::getTickCount();
+
+    double mat_time = (mat_t1 - mat_t2)/ (double)cv::getTickFrequency();
+
+    std::cout << "mat_time: " << mat_time << "sec" << std::endl;
+
+    //cv::imshow("720p", src);
+    //cv::waitKey();
+    return 0;
+}
