ffb70d4f32ffe6152e9e7268bf6744f69cf45d50
Weiyuan <ax61316@hotmail.com>
2018-01-27 17:48:06 +0800

    Update newest histogram equalization, powered by Medicine Yeh

--- histogram_equalization/main.cpp
+++ histogram_equalization/main.cpp
@@ -3,138 +3,123 @@
 Author: Wei-Yuan Alex Hsu
 Date:   2018/1/19 Fri
 Target: implement non-parallel and parallel( OpenCL based ) histogram equalization
-Reference: 
-[1] Histogram Calculation - https://docs.opencv.org/2.4/doc/tutorials/imgproc/histograms/histogram_calculation/histogram_calculation.html#code
-[2] How to scan images, lookup tables and time measurement with OpenCV - https://docs.opencv.org/2.4/doc/tutorials/core/how_to_scan_images/how_to_scan_images.html#howtoscanimagesopencv
+Reference:
+[1] Histogram Calculation -
+https://docs.opencv.org/2.4/doc/tutorials/imgproc/histograms/histogram_calculation/histogram_calculation.html#code
+[2] How to scan images, lookup tables and time measurement with OpenCV -
+https://docs.opencv.org/2.4/doc/tutorials/core/how_to_scan_images/how_to_scan_images.html#howtoscanimagesopencv
 
 */
 //--------------------------------------------------------------
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "main.hpp"
 #include <numeric>
-#include <vector>  /* std container - vector*/
-// opencv library
+#include <vector>
+#include <unistd.h>
+//#include <chrono>  // c++11
+
+// OpenCV Library
 #include "opencv2/opencv.hpp"
 #include "opencv2/core/ocl.hpp"
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
-void equalOfHist( cv::Mat& src);
-int main()
+#include "main.hpp"
+
+namespace CPU
 {
-    /*    
-    std::string INPUT_FILE_PATH ("/home/alex504/img_and_video_data_set/image");
-    std::string INPUT_FILENAME ("720p.jpg");
-    cv::Mat src = cv::imread(INPUT_FILE);       
-    */
-    std::string INPUT_FILE_PATH ("/home/alex504/img_and_video_data_set/video/1min");
-    //std::string INPUT_FILENAME ("car.mp4");     
-    std::string INPUT_FILENAME ("1440p_1min.mp4"); 
-       
-    std::string INPUT_FILE = INPUT_FILE_PATH + '/' + INPUT_FILENAME;
- 
-    // mini test
-    // cv::Mat input_grey = (cv::Mat_<float>(5,5) << 4, 4, 4, 4, 4,
-    //                                             3, 5, 5, 5, 3,
-    //                                             3, 4, 5, 4, 3,
-    //                                             4, 4, 4, 4, 4);
-    // cv::Mat grey_resize;
-    // input_grey.convertTo(grey_resize, CV_8U);
-    // std::cout <<"grey_resize type: "<< grey_resize.type() << std::endl;    
-    
-    //---------------------------------------// 
-    // histogram equalization
-    //---------------------------------------//           
-    // double equalOfHist_t1 = cv::getTickCount();    
-    // equalOfHist(grey);
-    // double equalOfHist_t2 = cv::getTickCount();
 
-    // double equalOfHist_time = (equalOfHist_t2 - equalOfHist_t1)/ cv::getTickFrequency();    
-    // std::cout <<"equalOfHist_time: "<< equalOfHist_time << std::endl;
-    
-    // waitKey(0) will display the window infinitely 
-    // until any keypress (it is suitable for image display)
-    //cv::waitKey(0);
-    
-    // mat type is CV_8U
-    cv::VideoCapture video(INPUT_FILE);    
-    // check video file
-    if( !video.isOpened() )
-    {
-        std::cout << "Could not initialize capturing..." << std::endl;
-        return -1;
-    }    
+void run_equalizeHist(cv::VideoCapture video)
+{
+    video.set(cv::CAP_PROP_POS_FRAMES, 0);
 
-    int frameNumbers = video.get(cv::CAP_PROP_FRAME_COUNT);
-    std::cout << "frameNumbers: " << frameNumbers << std::endl;
-    
-    std::vector<double> mat_time(frameNumbers);
-    std::vector<double> umat_time(frameNumbers);
-
-    int cnt = 0;
-    cv::Mat frame, grey;    
-    // non-opencl time measurement    
-    double mat_eH_outside_t1 = cv::getTickCount();    
-    for(;;)
-    {
-        video >> frame; // get a new frame from video    
-
-        cv::cvtColor(frame, grey, cv::COLOR_BGR2GRAY);    
+    //auto start = std::chrono::system_clock::now();
+    double start = (double)cv::getTickCount();
+    for (;;) {
+        cv::Mat frame, grey;
         cv::Mat eqOutput;
-        //double mat_eh_inside_t1 = cv::getTickCount();
+        if (video.read(frame) == false) break;
+
+        cv::cvtColor(frame, grey, cv::COLOR_BGR2GRAY);
         cv::equalizeHist(grey, eqOutput);
-        //double mat_eh_inside_t2 = cv::getTickCount();                
-
-        // Time result
-        //double mat_eH_time = (mat_eh_inside_t2 - mat_eh_inside_t1)/ cv::getTickFrequency();
-        //mat_time.push_back(mat_eH_time);                  
-        
-        cnt++;
-        if(cnt == frameNumbers) break;
     }
-    double mat_eH_outside_t2 = cv::getTickCount();    
-    // total time
-    double mat_total_time = (mat_eH_outside_t2 - mat_eH_outside_t1) / cv::getTickFrequency();
-    // fps
-    int fps = frameNumbers / mat_total_time;     
-    
-    std::cout <<"mat_total_time: "<< mat_total_time << std::endl;
-    std::cout <<"EqualizeHist() - fps of Mat: "<< fps << std::endl;
+    //auto end = std::chrono::system_clock::now();
+    double end = (double)cv::getTickCount();
 
-    // input video file again
-    cv::VideoCapture cap(INPUT_FILE);    
+    //std::chrono::duration<double> elapsed_seconds = end - start;
+    double elapsed_seconds = (end - start) / cv::getTickFrequency();
 
-    cnt = 0;
-    cv::Mat uframe, umat_grey;
-    // with opencl time measurement    
-    double umat_eH_outside_t1 = cv::getTickCount();    
-    for(;;)
-    {
-        cap >> uframe;    
+/*
+    std::cout << "Elapse Time: " << elapsed_seconds.count() << std::endl;
+    std::cout << "FPS " << video.get(cv::CAP_PROP_FRAME_COUNT) / elapsed_seconds.count()
+              << std::endl;
+*/              
+    std::cout << "Elapse Time: " << elapsed_seconds << std::endl;
+    std::cout << "FPS " << video.get(cv::CAP_PROP_FRAME_COUNT) / elapsed_seconds
+              << std::endl;    
+}
+}
 
-        cv::cvtColor(uframe, umat_grey, cv::COLOR_BGR2GRAY);
-        // Time measurement    
-        cv::UMat umat_eqOutput;
-        //double opencv_umat_equalizeHist_t1 = cv::getTickCount();
-        cv::equalizeHist(umat_grey, umat_eqOutput);
-        //double opencv_umat_equalizeHist_t2 = cv::getTickCount();
+namespace GPU
+{
 
-        //double opencv_umat_equalizeHist_time = (opencv_umat_equalizeHist_t2 - opencv_umat_equalizeHist_t1)/ cv::getTickFrequency();    
-        //umat_time.push_back(opencv_umat_equalizeHist_time);
+void run_equalizeHist(cv::VideoCapture video)
+{
+    video.set(cv::CAP_PROP_POS_FRAMES, 0);
 
-        cnt++;
-        if(cnt == frameNumbers) break; 
+    //auto start = std::chrono::system_clock::now();
+    double start = (double)cv::getTickCount();
+
+    int frameNumber = video.get(cv::CAP_PROP_FRAME_COUNT);
+    #pragma omp parallel for
+    for (int i = 0; i < frameNumber; i++) {
+        cv::UMat frame, grey;
+        cv::UMat eqOutput;
+        #pragma omp critical
+        {
+            video.read(frame);
+        }
+        //if (video.read(frame) == false) break;
+
+        cv::cvtColor(frame, grey, cv::COLOR_BGR2GRAY);
+        cv::equalizeHist(grey, eqOutput);
     }
-    double umat_eH_outside_t2 = cv::getTickCount();    
-    // total time
-    double umat_total_time = (umat_eH_outside_t2 - umat_eH_outside_t1) / cv::getTickFrequency();
-    // fps
-    fps = frameNumbers / umat_total_time; 
-    
-    std::cout <<"umat_total_time: "<< umat_total_time << std::endl;
-    std::cout <<"EqualizeHist() - fps of UMat: "<< fps << std::endl;
-    
+    //auto end = std::chrono::system_clock::now();
+    double end = (double)cv::getTickCount();
+
+    double elapsed_seconds = (end - start) / cv::getTickFrequency();
+/*
+    std::cout << "Elapse Time: " << elapsed_seconds.count() << std::endl;
+    std::cout << "FPS " << video.get(cv::CAP_PROP_FRAME_COUNT) / elapsed_seconds.count()
+              << std::endl;
+*/
+    std::cout << "Elapse Time: " << elapsed_seconds << std::endl;
+    std::cout << "FPS " << video.get(cv::CAP_PROP_FRAME_COUNT) / elapsed_seconds
+              << std::endl;               
+}
+}
+
+int main(int argc, char *argv[])
+{
+    std::string file_path ("../../img_and_video_data_set/video/1min");
+    std::string file_name ("720p_1min.mp4");
+    std::string input_file_video = file_path + '/' + file_name;
+
+    std::string input_file = (argc == 2) ? std::string(argv[1]) : input_file_video;
+
+    cv::VideoCapture video(input_file);
+
+    // check video file
+    if (!video.isOpened()) {
+        std::cout << "File not found: " << input_file << std::endl;
+        exit(1);
+    }
+
+    std::cout << "frameNumbers: " << video.get(cv::CAP_PROP_FRAME_COUNT) << std::endl;
+
+    CPU::run_equalizeHist(video);
+    GPU::run_equalizeHist(video);
+
     return 0;
 }
